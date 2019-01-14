23fce2fd1ef806b446236ceb2d78cd49d0022dff
customer <zhaifudong@qiyuexinxi.cn>
2019-01-14 10:46:24 +0800

    测试jenkins上传至github后自动部署

--- src/main/java/com/example/demo/DemoJenkinsServerApplication.java
+++ src/main/java/com/example/demo/DemoJenkinsServerApplication.java
@@ -8,6 +8,7 @@ public class DemoJenkinsServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenkinsServerApplication.class, args);
+		System.out.println("当前时间戳="+System.currentTimeMillis());
 	}
 
 }
